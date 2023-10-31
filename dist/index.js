690d6391b5b22a4f1d21670215f38bc1f0e4935f
reneoun <43538982+reneoun@users.noreply.github.com>
2023-10-31 11:58:01 +0100

    f

--- dist/index.js
+++ dist/index.js
@@ -30133,13 +30133,11 @@ const run = async () => {
   const octokit = github.getOctokit(githubToken);
 
   const { context } = github;
-  console.log("Context: ", context);
 
   const { pull_request: pullRequest } = context.payload;
-  console.log("Pull Request: ", pullRequest);
-  console.log(" Octokit: ", octokit);
+  console.log("Pull Request: ", pullRequest?.body);
 
-  await octokit.issues.createComment({
+  await octokit.rest.issues?.createComment({
     ...context.repo,
     issue_number: pullRequest.number,
     body: "Hello World!",
