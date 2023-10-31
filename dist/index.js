1bd47951ffa6a45e5b2fbc52e4b6c19dbce09792
reneoun <43538982+reneoun@users.noreply.github.com>
2023-10-31 13:04:30 +0100

    f

--- dist/index.js
+++ dist/index.js
@@ -30129,12 +30129,26 @@ const { context } = __nccwpck_require__(5438);
 
 const run = async () => {
   const githubToken = core.getInput("GITHUB_TOKEN");
-  console.log("Github Token: ", githubToken);
-
   const octokit = github.getOctokit(githubToken);
 
+  const { owner, repo } = context;
   const { pull_request } = context.payload;
-  console.log("Pull Request: ", pull_request, pull_request?.body);
+  // console.log("Pull Request: ", pull_request, pull_request?.body);
+  console.log("Pull Request: ", pull_request?.issue_number);
+  console.log(" Owner: ", owner, " Repo: ", repo);
+
+  await octokit.request(
+    `POST /repos/${owner}/${repo}/issues/${pull_request.issue_number}/comments`,
+    {
+      owner: owner,
+      repo: repo,
+      issue_number: pull_request.issue_number,
+      body: "Me too",
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
 
   await octokit.rest.issues?.createComment({
     ...context.repo,
--- src/action.js
+++ src/action.js
@@ -4,12 +4,26 @@ const { context } = require("@actions/github");
 
 const run = async () => {
   const githubToken = core.getInput("GITHUB_TOKEN");
-  console.log("Github Token: ", githubToken);
-
   const octokit = github.getOctokit(githubToken);
 
+  const { owner, repo } = context;
   const { pull_request } = context.payload;
-  console.log("Pull Request: ", pull_request, pull_request?.body);
+  // console.log("Pull Request: ", pull_request, pull_request?.body);
+  console.log("Pull Request: ", pull_request?.issue_number);
+  console.log(" Owner: ", owner, " Repo: ", repo);
+
+  await octokit.request(
+    `POST /repos/${owner}/${repo}/issues/${pull_request.issue_number}/comments`,
+    {
+      owner: owner,
+      repo: repo,
+      issue_number: pull_request.issue_number,
+      body: "Me too",
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
 
   await octokit.rest.issues?.createComment({
     ...context.repo,
