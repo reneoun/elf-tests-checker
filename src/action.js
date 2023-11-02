309bc1b8a97e19272145f00a09943e1f091cb0a4
reneoun <43538982+reneoun@users.noreply.github.com>
2023-11-02 14:34:03 +0100

    Test Checker

--- src/action.js
+++ src/action.js
@@ -1,82 +1,33 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const { context } = require("@actions/github");
-const { exec } = require("child_process");
 
-function extractFunctions(tsCode) {
-  const functionRegex =
-    /(public|private|protected)?\s+(static)?\s*(\w+)\s*\([^]*?\)\s*:?[^]*?{/g;
-  const functions = [];
-  let match;
-
-  while ((match = functionRegex.exec(tsCode)) !== null) {
-    functions.push(match[0]);
-  }
-
-  return functions;
-}
-
-const newLines = async (fileName) => {
-  const gitCMD = exec(` git diff origin/main -- ${fileName}`);
-  return new Promise((resolve, reject) => {
-    gitCMD.stdout.on("data", (data) => {
-      if (data.trim() === "") resolve([]);
-      resolve(
-        data
-          .split("\n")
-          .filter(
-            (newCode) =>
-              newCode.trim().startsWith("+") &&
-              !newCode.trim().startsWith("+++")
-          )
-          .map((line) => line.replace("+", ""))
-      );
-    });
-  });
-};
-
+const sad_emoticons = ["😭", "😢", "😞", "😔", "😟", "😕", "🙁", "☹️", "😣"];
 const run = async () => {
   const githubToken = core.getInput("GITHUB_TOKEN");
   const octokit = github.getOctokit(githubToken);
 
+  let inputCoverageMain = core.getInput("coverage-main") ?? null;
+  let inputCoverageBranch = core.getInput("coverage-branch") ?? null;
+
   const { owner, repo } = context.repo;
   const { pull_request } = context.payload;
 
-  let resultInComment = "";
-
-  const changedFiles = core.getInput("CODE_DIFF").replace(/'/g, "").split(" ");
-  const relevantChangedFiles = changedFiles.filter(
-    (file) => file.endsWith(".js") || file.endsWith(".ts")
-  );
-  console.log(" Git Diff", changedFiles);
-
-  for (const changedFile of relevantChangedFiles) {
-    resultInComment += `- *File:* ${changedFile} \n`;
-
-    try {
-      let lines = await newLines(changedFile);
-      let tsCode = lines.join("\n");
-      const functions = extractFunctions(tsCode);
-      resultInComment += `- ${functions.length} New Function(s)⚒️\n`;
-      functions.forEach((func) => {
-        resultInComment += `    - ${func.replace("\n", "")}\n`;
-      });
-      console.log(`Functions in ${changedFile}: `, functions);
-    } catch (error) {
-      console.log("Error in " + changedFile + ": ", error);
-    }
-  }
-
   try {
-    //TODO: Add a check to see if the comment already exists
-    const newPRComment = await octokit.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number: pull_request.number,
-      body: resultInComment,
-    });
+    console.log("inputCoverageMain", inputCoverageMain);
+    console.log("inputCoverageBranch", inputCoverageBranch);
+
+    const listArtifactsResponse =
+      await octokit.rest.actions.listWorkflowRunArtifacts({
+        owner,
+        repo,
+        run_id: pull_request.head.sha,
+      });
+    console.log("listArtifactsResponse", listArtifactsResponse);
   } catch (error) {
-    console.log("Error: ", error);
+    const sad_emoticon =
+      sad_emoticons[Math.floor(Math.random() * sad_emoticons.length)];
+    console.log(`Error[${sad_emoticon}]: ${error}`);
   }
 };
 
