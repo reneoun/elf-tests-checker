8805b40dc9d35a2bcc095e6af4b15a071e27d089
reneoun <43538982+reneoun@users.noreply.github.com>
2023-11-06 16:45:35 +0100

    Test Checker

--- dist/index.js
+++ dist/index.js
@@ -37497,6 +37497,14 @@ module.exports = require("fs");
 
 /***/ }),
 
+/***/ 3292:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("fs/promises");
+
+/***/ }),
+
 /***/ 3685:
 /***/ ((module) => {
 
@@ -37714,6 +37722,7 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const { context } = __nccwpck_require__(5438);
 const parser = __nccwpck_require__(4363);
+const fs = __nccwpck_require__(3292);
 
 const sad_emoticons = ["😭", "😢", "😞", "😔", "😟", "😕", "🙁", "☹️", "😣"];
 const neutral_emoticons = ["😐", "😑", "😶", "🙄", "😒", "🤐", "😬"];
@@ -37737,32 +37746,103 @@ const getEmoji = (kindEmo) => {
   return emoji;
 };
 
+const getRelevantValues = (doc) => {
+  let output = {};
+
+  let coverageInfo = doc.querySelectorAll("div.fl.pad1y.space-right2");
+  for (const infoItem of coverageInfo) {
+    const infoName = infoItem.querySelector("span.quiet").innerText;
+    const infoValuePct =
+      Number(infoItem.querySelector("span.strong").innerText.replace("%", "")) /
+      100;
+    const infoFraction = infoItem.querySelector("span.fraction").innerText;
+    const infoCovered = infoFraction.split("/")[0];
+    const infoTotal = infoFraction.split("/")[1];
+    output[infoName] = {
+      valuePct: infoValuePct,
+      covered: infoCovered,
+      total: infoTotal,
+    };
+  }
+
+  return output;
+};
+
+const createTable = (covMap) => {
+  let table = [];
+  let tableHeader = [];
+  let tableBody = [];
+
+  const containsHeader = (header) => {
+    return tableHeader.map((h) => h.data).includes(header);
+  };
+
+  for (const [key, value] of covMap.entries()) {
+    for (const [key2, value2] of Object.entries(value)) {
+      let newRow = [];
+      const coverageContext = `${key} - ${key2}`;
+      if (!containsHeader("Coverage Context")) {
+        tableHeader.push({ data: "Coverage Context", header: true });
+      }
+      newRow.push(coverageContext);
+      for (const [key3, value3] of Object.entries(value2)) {
+        if (!containsHeader(key3)) {
+          tableHeader.push({ data: key3, header: true });
+        }
+        newRow.push(value3);
+      }
+      tableBody.push(newRow);
+    }
+  }
+
+  table.push(Array.from(new Set(tableHeader)));
+  table.push(tableBody);
+
+  return table;
+};
+
 const run = async () => {
-  const githubToken = core.getInput("GITHUB_TOKEN");
-  const octokit = github.getOctokit(githubToken);
+  const coverageMap = new Map();
+  // const githubToken = core.getInput("GITHUB_TOKEN");
+  // const octokit = github.getOctokit(githubToken);
 
   let inputCoverageMain = core.getInput("coverage-main") ?? null;
   let inputCoverageBranch = core.getInput("coverage-branch") ?? null;
 
+  //FOR TESTING
+  // let inputCoverageMain;
+  // inputCoverageMain = await fs.readFile("src/main-test-coverage.html", {
+  //   encoding: "utf8",
+  // });
+
+  // let inputCoverageBranch;
+  // inputCoverageBranch = await fs.readFile("src/pr-test-coverage.html", {
+  //   encoding: "utf8",
+  // });
+
+  console.log("Main", inputCoverageMain);
+  console.log("Branch", inputCoverageBranch);
+
   if (inputCoverageMain === null || inputCoverageBranch === null) {
     core.notice(`No coverage files found. Exiting. ${getEmoji("neutral")}`);
+    // console.log(`No coverage files found. Exiting. ${getEmoji("neutral")}`);
     return 0;
   }
 
   let covMainDoc = parser.parse(inputCoverageMain);
   let covBranchDoc = parser.parse(inputCoverageBranch);
 
-  console.log("Main", covMainDoc);
-  console.log("Branch", covBranchDoc);
-
-  const { owner, repo } = context.repo;
-  const { pull_request } = context.payload;
-
-  core.summary.addHeading("Coverage Report ☑️📃").write();
-
   try {
-    console.log("Main as Doc", covMainDoc);
-    console.log("Branch as Doc", covBranchDoc);
+    coverageMap.set("main", getRelevantValues(covMainDoc));
+    coverageMap.set("branch", getRelevantValues(covBranchDoc));
+
+    console.log("MAIN", coverageMap.get("main"));
+    console.log("BRANCH", coverageMap.get("branch"));
+
+    let sumTable = createTable(coverageMap);
+    console.log("SUMTABLE", sumTable);
+
+    core.summary.addHeading("Coverage Report ☑️📃").addTable(sumTable).write();
   } catch (error) {
     console.log(`Error[${getEmoji("sad")}]: ${error}`);
   }
